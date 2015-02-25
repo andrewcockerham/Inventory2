a03f8bf718f1a9e2551b372259ea171ed5383783
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-02-25 11:05:58 -0600

    debugging employee id

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -146,7 +146,9 @@ class LotsController < ApplicationController
         p params["lot"]["build_lots_attributes"]#["employee_id"]
         # p params["lot"]["build_lots_attributes"].last
         p params["lot"]["build_lots_attributes"].length
-        p params["lot"]["build_lots_attributes"][(params["lot"]["build_lots_attributes"].length - 1).to_s]
+        p params["lot"]["build_lots_attributes"][(params["lot"]["build_lots_attributes"].length - 1).to_s]["employee_id"]
+
+        #@lot.build_lots.employee_id =
 
         format.html { redirect_to @lot, notice: 'Lot was successfully updated.' }
         format.json { head :no_content }
