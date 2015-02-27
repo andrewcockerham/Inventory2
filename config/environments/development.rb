193281e6f8406c7c270c08e0cfaea28c3bb753ce
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-02-27 09:52:27 -0600

    setup action_mailer defaults

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Inventory2::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  ## for Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,9 @@ Inventory2::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # For Devise
+  config.action_mailer.default_url_options = { host: 'tvainventory.herokuapp.com' }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
